291b4d915c5fb6ee85aa03557ed0c9bfd30ba11b
Benjamin Kosten <benkosten@gmail.com>
2017-02-24 00:55:27 -0500

    Add authentication via SHA-256 encryption.

--- Server/server.py
+++ Server/server.py
@@ -3,23 +3,59 @@ from tornado.ioloop import IOLoop
 from tornado import gen
 import time
 import json
+import hashlib
+import random
 
-import json
-
-users = {}
+user_info = {}
+user_data = {}
 
 def init():
-    with open('users.json', 'r') as data_file:
-        users = json.load(data_file)
+    with open('users/user_info.json', 'r') as data_file:
+        user_info = json.load(data_file)
+        data_file.close()
 
-def create_user(name, passwd):
-    if name not in users:
-        users[name] = {
-            "pass": passwd,
-            }
-        with open('users.json', 'w') as outfile:
-            json.dump(users, outfile)
+    with open('users/user_data.json', 'r') as data_file:
+        user_data = json.load(data_file)
+        data_file.close()
+
+def create_salt():
+    alphanum = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    chars = []
+
+    for i in range(16):
+        chars.append(random.choice(alphanum))
+
+    b = ""
+    for i in range(len(chars)):
+        b = b + str(chars[i])
+
+    return b
+
+def hash_SHA256(pwd):
+    hash_object = hashlib.sha256(pwd)
+    hex_dig = hash_object.hexdigest()
+    return hex_dig
+
+def create_user(name, pwd):
+    if name not in user_info:
+        user_info[name] = {
+            'hash': hash_SHA256(pwd),
+            'salt': create_salt(),
+        }
+        user_data[name] = {
+            'money': 100
+        }
+
+        with open('users/user_info.json', 'w') as outfile:
+            json.dump(user_info, outfile)
+            outfile.close()
+
+        with open('users/user_data.json', 'w') as outfile:
+            json.dump(user_data, outfile)
+            outfile.close()
+
         return True
+
     else:
         return "You dun goofed. (Randolph Xia, 2017)"
 
@@ -31,10 +67,16 @@ class UserHandler(tornado.web.RequestHandler):
         passwd = self.get_argument('password')
         response = create_user(name, passwd)
         self.write(str(response))
-        self.finish
+        self.finish()
+
+    def get(self):
+        name = self.get_argument('user')
+        response = user_data[name]
+        self.write(response)
+        self.finish()
 
 application = tornado.web.Application([
-    (r"/createUser", UserHandler),
+    (r"/user", UserHandler),
     ])
 
 if __name__ == '__main__':
